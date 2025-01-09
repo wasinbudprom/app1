75e64aeafa39750a869ead46677f9f65f6b530c9
wasinbudprom <wasin.budprom@hotmail.com>
2025-01-09 11:36:29 +0700

    add class-components.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import './style.css'
 import { Header, Content, Footer } from './func-components';
+import Calendar from './class-components'
 
 function App() {
   return (
     <>
         <Header/>
+        <p><center><Calendar/></center></p>
         <Content/>
         <Footer/>
         {/* <h2>DeveloperThai.com</h2>
