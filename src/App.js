9176a5193c657b64755a88ab889b5df2f6e9f2d1
wasinbudprom <wasin.budprom@hotmail.com>
2025-01-09 10:56:23 +0700

    create func-components update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import './style.css'
+import { Header, Content, Footer } from './func-components';
 
 function App() {
   return (
     <>
-        <h2>DeveloperThai.com</h2>
-        <div className='title'>React & React Native</div>
+        <Header/>
+        <Content/>
+        <Footer/>
+        {/* <h2>DeveloperThai.com</h2>
+        <div className='title'>React & React Native</div> */}
     </>
   );
 }
