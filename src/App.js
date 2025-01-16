d908196c3cd64f944ac859302a5b68ec86a902d3
wasinbudprom <wasin.budprom@hotmail.com>
2025-01-16 11:36:15 +0700

    eventData2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React from 'react';
 import './style.css'
 import {Button} from './class-components'
 import {Calculator2} from './Calculator';
+import {Eventdata} from './event-data'
 
 function App() {
   return (
     <>
-        <Button />
-        <Calculator2/>
+        <Eventdata/>
         {/* <h2>DeveloperThai.com</h2>
         <div className='title'>React & React Native</div> */}
     </>
